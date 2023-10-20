be4861a93286d2f2569a81371c05b25de2fea2e3
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-10-20 10:31:02 +0600

    midTermXM

---
+++ midtermPractice.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+// int main(){
+// //odd number in range
+
+// int s,e;
+// scanf("%d %d",&s, &e);
+// for(int i=s; i<e; i++){
+//     if (i%2!=0){
+//         printf("%d is the result",i);
+//     }
+// } 
+
+
+//  return 0;
+// }
+
+
+// int main(){
+// //even number in range
+
+// int s,e;
+// scanf("%d %d",&s, &e);
+// for(int i=s; i<e; i++){
+//     if (i%2==0){
+//         printf("%d is the result",i);
+//     }
+// } 
+
+
+//  return 0;
+// }
+
+int main(){
+//max/mini number in range
+
+int a,b,c;
+printf("Enter three numbers: ");
+scanf("%d %d",&a, &a,&c);
+
+//finding the max number
+
+int max = a;
+if(b > max) {
+    max = b;
+}
+if (c > max) {
+max = c;
+}
+
+//finding the minimum 
+
+if ()
+
+
+
+ return 0;
+}
