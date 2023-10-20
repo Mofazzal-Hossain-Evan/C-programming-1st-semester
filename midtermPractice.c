010aa0fcbb5675b4ea170de225d7964864a398ab
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-10-20 10:42:36 +0600

    m2

--- midtermPractice.c
+++ midtermPractice.c
@@ -35,7 +35,7 @@ int main(){
 
 int a,b,c;
 printf("Enter three numbers: ");
-scanf("%d %d",&a, &a,&c);
+scanf("%d %d %d",&a, &b,&c);
 
 //finding the max number
 
@@ -48,10 +48,16 @@ max = c;
 }
 
 //finding the minimum 
+int min = a;
+if (b < min) {
+    min = b;
+}
+if (c < min) {
+    min = c;
+}
 
-if ()
-
-
+    printf("Max number is: %d\n", max);
+    printf("Min number is: %d\n", min);
 
  return 0;
 }
