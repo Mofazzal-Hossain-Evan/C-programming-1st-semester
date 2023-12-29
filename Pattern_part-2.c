d3648e8e2b9b714449ebc7559d863462bb450e22
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2023-12-29 08:41:18 +0600

    complete

--- Pattern_part-2.c
+++ Pattern_part-2.c
@@ -2,13 +2,46 @@
 
  int main() {
 
+    // int n, row, col;
+    // printf("Enter n = ");
+    // scanf("%d", &n);
+    // for(row=1; row<=n; row++) {
+     
+    //  for(col=1; col<=row; col++){
+    //     printf("%d", row%2);
+    //  }
+    //     printf("\n");
+    // }
+
+
+
+
+
+
+
+
+
+
+    // int n, row, col;
+    // printf("Enter n = ");
+    // scanf("%d", &n);
+    // for(row=1; row<=n; row++) {
+     
+    //  for(col=1; col<=row; col++){
+    //     printf("%c", col+96);
+    //  }
+    //     printf("\n");
+    // }
+
+
+
     int n, row, col;
     printf("Enter n = ");
     scanf("%d", &n);
     for(row=1; row<=n; row++) {
      
      for(col=1; col<=row; col++){
-        printf("%d", col%2);
+        printf("*");
      }
         printf("\n");
     }
